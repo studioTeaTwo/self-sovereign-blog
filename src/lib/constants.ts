d1d7ec12f5b10249f8010feac424700084b44083
T2 <studioTeaTwo@gmail.com>
2023-12-01 12:51:55 +0900

    feat: bitcoin donation

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,4 @@ import { dev } from '$app/environment';
 export const Title = 'Self Sovereign Blog';
 export const RepositoryUrl = 'https://github.com/studioTeaTwo/self-sovereign-blog';
 export const L402server = dev ? 'http://localhost:8180' : '';
+export const BitcoinDonationAddress = 'bc1q8pmsy0xfrkpr3vky9kw5kjmqrm0fsg70q963dg';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,26 @@
 <script>
-	import { Title, RepositoryUrl } from '$lib/constants';
+	import { Title, RepositoryUrl, BitcoinDonationAddress } from '$lib/constants';
 	import { base } from '$app/paths';
 	import GithubLogo from '$lib/assets/GitHub-Mark-32px.png';
+	import BitcoinDonation from '$lib/assets/Bitcoin_Donation.png';
+
+	let dialog;
+	
+	/** @param {MouseEvent} event */
+	function handleClickBitcoin(event) {
+		navigator.clipboard.writeText(BitcoinDonationAddress);
+		dialog.showModal();
+		// close if clicked outside the modal
+		dialog.addEventListener('click', function (event) {
+			if (event.target === dialog) {
+				dialog.close();
+			}
+		});
+	}
+	/** @param {MouseEvent} event */
+	function handleClickClose(event) {
+		dialog.close();
+	}
 </script>
 
 <header>
@@ -15,7 +34,23 @@
 <footer>
 	<p>© studioTeaTwo</p>
 	<p><a href={RepositoryUrl} target="”_blank”"><img alt="Github logo" src={GithubLogo} /></a></p>
+	<p>
+		<button type="button" class="bitcoin-donation" on:click={handleClickBitcoin}
+			><img alt="Donation" src={BitcoinDonation} width="32" /></button
+		>
+	</p>
 </footer>
+
+<dialog bind:this={dialog}>
+	<div class="donation">
+		<div class="donation-close">
+			<button on:click={handleClickClose}>x</button>
+		</div>
+		<p>Thank you for your love!</p>
+		<div class="donation-address-qr"><img alt="Donation" src={BitcoinDonation} /></div>
+		<div class="donation-address-text"><span>{BitcoinDonationAddress}</span></div>
+	</div>
+</dialog>
 
 <style>
 	:global(html, body) {
@@ -45,5 +80,20 @@
 		justify-content: center;
 		column-gap: 16px;
 		margin-top: auto;
+	}
+	.bitcoin-donation {
+		border: transparent;
+		background-color: transparent;
+	}
+	.donation {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.donation-close {
+		margin-left: auto;
+	}
+	.donation-address-text {
+		font-size: 0.8rem;
 	}
 </style>
